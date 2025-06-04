49b82f77c26a4d5b2b8bfb87cbc51d6134a6a3e4
Codeweaverai <taylorwiker8@gmail.com>
2025-06-04 17:59:47 +0200

    Update CourseDetailPage.tsx

--- src/pages/learning/CourseDetailPage.tsx
+++ src/pages/learning/CourseDetailPage.tsx
@@ -731,7 +731,7 @@ const CourseDetailPage = () => {
                             </Button>
                             <Button variant="outline" size="sm">
                               <Mail className="h-4 w-4 mr-2" />
-                              Schedule Consultation
+                              Send Message
                             </Button>
                           </div>
                         </div>
