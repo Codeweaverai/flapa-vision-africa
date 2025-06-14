e605825c148485a0eb1b01b73677ee308258b1bf
Codeweaverai <taylorwiker8@gmail.com>
2025-06-14 15:37:10 +0200

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -10,6 +10,7 @@ import CoursesSection from '@/components/home/CoursesSection';
 import MediaSection from '@/components/home/MediaSection';
 import CommunityAISection from '@/components/home/CommunityAISection';
 import CallToAction from '@/components/home/CallToAction';
+import CallToAction from '@/components/home/LearningSection';
 
 const HomePage = () => {
   return (
@@ -21,6 +22,7 @@ const HomePage = () => {
       <CommunityAISection />
       <EventsSection />
       <PastEventsSection />
+      <LearningSection />
       <MediaSection />
       <CallToAction />
     </Layout>
