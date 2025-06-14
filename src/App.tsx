e67dc96a5332362df13b80059accfb4ba839e5dd
Codeweaverai <taylorwiker8@gmail.com>
2025-06-14 14:43:47 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -142,7 +142,7 @@ function App() {
                   <Route path="/courses" element={<CoursesPage />} />
                   <Route path="/courses/:id" element={<CourseDetailPage />} />
                   <Route path="/events" element={<EventsPage />} />
-                  <Route path="/events/:eventId" element={<EventDetailPage />} />
+                  <Route path="/event-detail/:eventId" element={<EventDetailPage />} />
                   <Route path="/about" element={<AboutPage />} />
                   <Route path="/contact" element={<ContactPage />} />
                   <Route path="/learning" element={<LearningPage />} />
