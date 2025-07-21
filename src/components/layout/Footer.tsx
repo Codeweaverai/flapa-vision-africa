21d086fbf6a9becd6414a19a1c0d4c149378c477
Codeweaverai <taylorwiker8@gmail.com>
2025-07-21 16:58:46 +0200

    Update Footer.tsx

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -98,9 +98,6 @@ const Footer = () => {
               <Link to="/terms" className="hover:text-primary transition-colors">
                 Terms of Service
               </Link>
-              <Link to="/verify" className="hover:text-primary transition-colors">
-                Verify SkillPulse Certificate
-              </Link>
             </div>
           </div>
         </div>
