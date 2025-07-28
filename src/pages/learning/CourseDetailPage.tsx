071f4a5b7f8ce5235cbf21dd45f1850bff2f9268
Codeweaverai <taylorwiker8@gmail.com>
2025-07-28 09:56:03 +0200

    Update CourseDetailPage.tsx

--- src/pages/learning/CourseDetailPage.tsx
+++ src/pages/learning/CourseDetailPage.tsx
@@ -554,7 +554,7 @@ const CourseDetailPage = () => {
                           controls={true}
                           playing={false}
                           width="100%"
-                          height="90%"
+                          height="100%"
                           light={course.thumbnail_url}
                           config={{
                             file: {
