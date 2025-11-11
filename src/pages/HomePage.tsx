754eb1933ecc4947b4b1dc2c7453e06b2bd14fd3
Codeweaverai <taylorwiker8@gmail.com>
2025-11-11 16:26:25 +0200

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -12,7 +12,7 @@ import BrowseCategoriesSection from '@/components/home/BrowseCategoriesSection';
 import CreatorsSection from '@/components/home/CreatorsSection';
 import TrendingNowSection from '@/components/home/TrendingNowSection';
 import LocalContentSection from '@/components/home/LocalContentSection';
-import LocalContentSection from '@/components/home/LiveEventsSection';
+import LiveEventsSection from '@/components/home/LiveEventsSection';
 
 const HomePage = () => {
   return (
